a055494e28f5d7431fdc443bb194e31760c65ae2
zach <zhedgepeth1124@gmail.com>
2016-10-18 11:15:12 -0400

    add a readme with a setup checklist and development commands

--- app/app.component.ts
+++ app/app.component.ts
@@ -2,8 +2,11 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'my-app',
-  template: <h1>My First Angular 2 App</h1>
-
+  template: `
+  <div class="container">
+    <h1>angular2-skeleton</h1>
+  </div>
+  `
 })
 
 export class AppComponent {
